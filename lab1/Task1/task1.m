b7be80559d32b1f4698ddfdb41774163fa06a880
mvhhampek <hampus.kamppi02@gmail.com>
2024-11-19 20:43:22 +0100

    fixad task 1 (fr)

--- lab1/Task1/task1.m
+++ lab1/Task1/task1.m
@@ -2,17 +2,15 @@ close all
 clear
 clc
 
-
-
 via = [
-  0, 0, 0, 0, 0, 0  
-  0, 0, 1, 0, pi/2, 0
-  1, 0, 1, 0, -pi, 0
-  1, 0, -0.5, 0, pi/2, 0
-];
+  0, 0, 0, 0, 0, 0  % A
+  0, 0, 1, 0, pi/2, 0 % B
+  1, 0, 1, 0, -pi, 0 % C
+  1, 0, -0.5, 0, pi/2, 0 % D
+]
 
+% to draw the 4 poses
 poses2 = repmat(SE3(), 1, 4); 
-
 for i = 1:4
     position = via(i, 1:3);
     rpy = via(i, 4:6);
@@ -20,8 +18,25 @@ for i = 1:4
     poses2(i) = SE3(position) * SE3.rpy(rpy(1), rpy(2), rpy(3));
 end
 
-vel = 1;
-traj = mstraj(via, [vel,vel,vel,vel,vel,vel], [], via(1,:), 0.1, 2);
+%%
+j = 5;
+for i = 2:size(via, 1)
+    diff = via(i, j) - via(i-1, j);
+    % map difference to [-pi, pi]
+    while diff > pi
+        diff = diff - 2*pi;
+    end
+    while diff < -pi
+        diff = diff + 2*pi;
+    end
+    via(i, j) = via(i-1, j) + diff;
+end
+
+
+traj = mstraj(via, [1,1,1,1,1,1], [], via(1,:), 0.1, 2);
+%%
+
+
 
 num_poses = size(traj, 1);
 % make list of empty se3 object
@@ -35,7 +50,7 @@ for i = 1:num_poses
     poses(i) = SE3(position) * SE3.rpy(rpy(1), rpy(2), rpy(3));
 end
 %%
-figure
+h = figure;
 hold on
 grid on
 axis equal
@@ -51,7 +66,7 @@ frame = [];
 
 % A,B,C,D poses...
 for i = 1:4
-    trplot(poses2(i), 'length', axis_length/2, 'arrow', 'rgb')
+    trplot(poses2(i), 'length', axis_length/2, 'arrow', 'rgb');
 end
 
 % draw each pose in the trajectory
@@ -61,8 +76,18 @@ for i = 1:num_poses
         delete(frame);
     end
     
-    frame = trplot(poses(i), 'length', axis_length, 'arrow', 'rgb');% 'rvis');
+    frame = trplot(poses(i), 'length', axis_length, 'arrow', 'rgb');
 
+
+    % save as gif...
+    %frame_image = getframe(h);
+    %im = frame2im(frame_image); 
+    %[imind, cm] = rgb2ind(im, 256); 
+    %if i == 1
+    %    imwrite(imind, cm, '123123.gif', 'gif', 'Loopcount', inf, 'DelayTime', 0.05);
+    %else
+    %    imwrite(imind, cm, '123123.gif', 'gif', 'WriteMode', 'append', 'DelayTime', 0.05);
+    %end
     pause(0.05)
 end
 
