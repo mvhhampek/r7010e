cd7cf7fd76fb8996ac209447aacc625cf98a02d7
mvhhampek <59012814+mvhhampek@users.noreply.github.com>
2024-11-29 16:25:32 +0100

    lab3 task 1-3

--- lab3/lab3_trajectory.m
+++ lab3/lab3_trajectory.m
@@ -1,14 +1,34 @@
 clear
 clc
 close all
+%% SQUARE LOOKING TOWARDS NEXT POINT 
+square = [
+    0 0 1 pi/2
+    0 1 1 2*pi
+    1 1 1 3*pi/2
+    1 0 1 -pi
+];
+x = square(:, 1);
+y = square(:, 2);
+z = square(:, 3);
+yaw = square(:, 4);
 
+%% CIRCLE LOOKING TOWARDS CENTER
 theta = linspace(0, 2*pi, 50);
 
 x = sin(theta);
 y = cos(theta);
-z = 1;
+z = ones(1, length(x));
 
 yaw = atan2(-y, -x);
+
+%% TILTED CIRCLE
+pos = [x' y' z']*rotx(45, 'deg');
+x = pos(:, 1);
+y = pos(:, 2);
+z = pos(:, 3);
+
+
 %%
 figure
 hold on
@@ -18,17 +38,21 @@ xlabel('X'); ylabel('Y'); zlabel('Z');
 xlim([-2, 2]);
 ylim([-2, 2]);
 zlim([ 0, 2]);
-view(45, 30);
+
+%view(45, 30); % normal
+view(135, 30); % tilted
 frame = [];
-for C = 1:100000
-    i = mod(C-1, 50) + 1;
-    pose = SE3(x(i),y(i),z) * SE3.rpy(0, 0, yaw(i));
+plot3(x, y, z, 'black--', 'LineWidth', 0.5);
+
+for C = 1:1000
+    i = mod(C-1, length(x)) + 1;
+    pose = SE3(x(i),y(i),z(i)) * SE3.rpy(0, 0, yaw(i));
 
     if ~isempty(frame)
         delete(frame);
     end
     
-    frame = trplot(pose, 'length', 0.5);
+    frame = trplot(pose, 'length', 0.3);
     
     pause(0.05)
 
